5514a771f4ed66c408ab6478fe3d617122988b53
Axel147 <lopez.b.axel@gmail.com>
2023-12-13 23:46:06 -0300

    se importo el sharedModule y storeModule para poder agregar el footer y header a todas las paginas

--- src/app/modules/principal.module.ts
+++ src/app/modules/principal.module.ts
@@ -5,6 +5,7 @@ import { AdminModule } from './admin/admin.module';
 import { HomeModule } from './public/home/home.module';
 import { PrincipalComponent } from './principal.component';
 import { StoreModule } from './public/store/store.module';
+import { SharedModule } from '@shared/shared.module';
 
 
 @NgModule({
@@ -15,8 +16,9 @@ import { StoreModule } from './public/store/store.module';
     AdminModule,
     CommonModule,
     HomeModule,
+    PrincipalRoutingModule,
+    SharedModule,
     StoreModule,
-    PrincipalRoutingModule
   ]
 })
 export class PrincipalModule { }
